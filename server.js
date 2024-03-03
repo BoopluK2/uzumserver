293cee5bdb8813afef34a6a48564490e7d84e2e5
BoopluK2 <110056464+BoopluK2@users.noreply.github.com>
2024-03-03 19:07:58 +0500

    aaaaa

--- server.js
+++ server.js
@@ -13,6 +13,20 @@ import path from 'path';
 dotenv.config();
 const __dirname = path.resolve();
 const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
+
 app.use(cors(process.env.ALLOWED_ORIGINS));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
